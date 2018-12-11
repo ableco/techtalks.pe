15b49842e0bc9adaab9714959bb1ef6c284e5cc6
Sergio Xalambrí <sergio@able.co>
2018-12-11 15:18:04 -0500

    Use dark button styles

--- components/button.js
+++ components/button.js
@@ -14,37 +14,38 @@ export function LinkButton({
     <a href={href} className={cn(className, "button", { invert })} {...props}>
       {children}
       <style jsx>{`
-        a {
+      .button {
           display: inline-block;
           font-size: 1em;
           cursor: pointer;
           text-decoration: none;
-          padding: 0.25rem 0.5rem;
+          padding: 0.5rem 0.5rem;
           margin: -0.25rem -0.5rem;
           border-radius: 7px;
-          color: #0076ff;
+          color: black;
           transition: background 0.2s ease, color 0.2s ease,
             box-shadow 0.2s ease;
         }
-        a:hover {
-          color: #0076ff;
-          background: rgba(0, 118, 255, 0.1);
+        .button:hover {
+          color: black;
+          background: rgba(11, 11, 11, 0.1);
         }
-        a.invert {
+        .button.invert {
           margin: 0;
           padding: 0 2rem;
           height: 2.5rem;
           line-height: 2.5rem;
           border-radius: 7px;
-          background: #0076ff;
-          box-shadow: 0 4px 14px 0 rgba(0, 118, 255, 0.39);
+          background: black;
+          box-shadow: 0 4px 14px 0 rgba(0, 0, 0, 0.39);
           color: white;
+          font-weight: 600;
         }
-        a.invert:hover {
-          background: rgba(0, 118, 255, 0.9);
-          box-shadow: 0 6px 20px rgba(0, 118, 255, 0.23);
+        .button.invert:hover {
+          background: rgba(11, 11, 11, 0.9);
+          box-shadow: 0 6px 20px rgba(0, 0, 0, 0.23);
         }
-        a.invert:active {
+        .button.invert:active {
           background: #006ae6;
         }
       `}</style>
@@ -67,40 +68,38 @@ export function ActionButton({ children, invert, className, ...props }) {
     >
       {children}
       <style jsx>{`
-        button {
-          background: none;
-          border: none;
-          font-size: 1em;
+        .button {
           display: inline-block;
+          font-size: 1em;
           cursor: pointer;
           text-decoration: none;
-          padding: 0.25rem 0.5rem;
+          padding: 0.5rem 0.5rem;
           margin: -0.25rem -0.5rem;
           border-radius: 7px;
-          color: #0076ff;
+          color: black;
           transition: background 0.2s ease, color 0.2s ease,
             box-shadow 0.2s ease;
-          outline: none;
         }
-        button:hover {
-          color: #0076ff;
-          background: rgba(0, 118, 255, 0.1);
+        .button:hover {
+          color: black;
+          background: rgba(11, 11, 11, 0.1);
         }
-        button.invert {
+        .button.invert {
           margin: 0;
           padding: 0 2rem;
           height: 2.5rem;
           line-height: 2.5rem;
           border-radius: 7px;
-          background: #0076ff;
-          box-shadow: 0 4px 14px 0 rgba(0, 118, 255, 0.39);
+          background: black;
+          box-shadow: 0 4px 14px 0 rgba(0, 0, 0, 0.39);
           color: white;
+          font-weight: 600;
         }
-        button.invert:hover {
-          background: rgba(0, 118, 255, 0.9);
-          box-shadow: 0 6px 20px rgba(0, 118, 255, 0.23);
+        .button.invert:hover {
+          background: rgba(11, 11, 11, 0.9);
+          box-shadow: 0 6px 20px rgba(0, 0, 0, 0.23);
         }
-        button.invert:active {
+        .button.invert:active {
           background: #006ae6;
         }
       `}</style>
