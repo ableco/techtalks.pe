5849e711e86ab00721b4a719726343c918c580d8
Sergio Xalambrí <sergio@able.co>
2018-12-12 10:27:05 -0500

    Fix attribute name

--- pages/index.js
+++ pages/index.js
@@ -14,7 +14,7 @@ function HomePage() {
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
         <title>Tech Talks - Tu Meetup</title>
-        <meta http-equiv="Content-Type" content="text/html;charset=UTF-8"/>
+        <meta httpEquiv="Content-Type" content="text/html;charset=UTF-8"/>
       </Head>
 
       <Hero />
