8cb76279aca83e4447ad35cf6b681281b3dd56ff
Sergio Xalambrí <sergio@able.co>
2018-12-14 17:34:06 -0500

    Add more meta tags

--- pages/index.js
+++ pages/index.js
@@ -23,6 +23,8 @@ function HomePage() {
         <meta name="MobileOptimized" content="320" />
         <meta name="robots" content="index, follow" />
         <meta name="theme-color" content="#000000" />
+        <meta name="apple-mobile-web-app-title" content="Tech Talks" />
+        <meta content="IE=edge,chrome=1" httpEquiv="X-UA-Compatible" />
       </Head>
 
       <Hero />
