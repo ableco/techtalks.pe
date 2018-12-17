a569249ad46935fe3bbc71a37d8b18dca8b7fb12
Sergio Xalambrí <sergio@able.co>
2018-12-17 12:01:35 -0500

    Remove old hero component and start adding next event info

--- components/sections/hero.js
+++ components/sections/hero.js
@@ -1,61 +1,39 @@
+import format from "date-fns/format";
+import locale from "date-fns/locale/es";
+
 import Navigation from "../navigation";
 import Name from "../name";
 import Slogan from "../slogan";
 import Button from "../ui/button";
-
+import { H2 } from "../ui/heading";
 import Wrapper from "../ui/wrapper";
+import events from "../../data/events.json";
+
+const lastEvent = events[0];
 
 function Hero() {
   return (
     <section id="hero" className="hero">
       <Navigation />
       <Wrapper>
-        <Name style={{ fontSize: "5rem" }} />
-        <Slogan />
-        <div className="button-container">
-          <Button href="https://goo.gl/forms/0XRxomZaK9gK13F03" invert>
-            Quiero dar una charla
-          </Button>
-        </div>
+        <header>
+          <div>
+            <Name />
+            <Slogan />
+            <div className="button-container">
+              <Button href="https://goo.gl/forms/0XRxomZaK9gK13F03" invert>
+                Quiero dar una charla
+              </Button>
+            </div>
+          </div>
+          {lastEvent.date !== "tbd" && (
+            <div>
+              <H2>Próximo Evento</H2>
+              <strong>{format(lastEvent.date, "[El] dddd DD [de] MMMM", { locale })}</strong>
+            </div>
+          )}
+        </header>
       </Wrapper>
-      <style jsx>{`
-        .hero {
-          background-color: white;
-          background-image: radial-gradient(
-            circle,
-            #d7d7d7,
-            #d7d7d7 1px,
-            #fff 1px,
-            #fff
-          );
-          background-size: 28px 28px;
-        }
-        .button-container {
-          margin-bottom: 2.5rem;
-        }
-        @media (min-width: 64em) {
-          .button-container {
-            margin-bottom: 5rem;
-          }
-        }
-      `}</style>
-    </section>
-  );
-}
-
-function OldHero() {
-  return (
-    <section id="hero" className="hero">
-      <Navigation />
-      <header>
-        <Name />
-        <Slogan />
-        <div className="button-container">
-          <Button href="https://goo.gl/forms/0XRxomZaK9gK13F03" invert>
-            Dar una charla
-          </Button>
-        </div>
-      </header>
       <style jsx>{`
         .hero {
           background-color: white;
@@ -70,10 +48,9 @@ function OldHero() {
         }
         header {
           display: flex;
+          justify-content: space-between;
+          align-items: baseline;
           flex-direction: column;
-          justify-content: center;
-          align-items: center;
-          text-align: center;
         }
         .button-container {
           margin-bottom: 2.5rem;
@@ -81,6 +58,9 @@ function OldHero() {
         @media (min-width: 64em) {
           .button-container {
             margin-bottom: 5rem;
+          }
+          header {
+            flex-direction: row;
           }
         }
       `}</style>
