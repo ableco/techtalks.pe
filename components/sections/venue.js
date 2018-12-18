ed6aa2b8f3dc42cc3a77155eb891c4c7ee2a0331
Sergio Xalambrí <sergio@able.co>
2018-12-18 12:55:18 -0500

    Simplify venue section with only the map

--- components/sections/venue.js
+++ components/sections/venue.js
@@ -5,67 +5,24 @@ import venue from "../../data/venue.json";
 
 function Venue() {
   return (
-    <Section id="venue">
+    <Section id="venue" inverted>
       <Wrapper center>
-        <H2>Venue</H2>
-        <div className="content-wrapper">
-          <iframe
-            src={venue.map}
-            height="200"
-            frameBorder="0"
-            allowFullScreen
-          />
-          <div className="content">
-            <h2>Ubicación</h2>
-            <address>
-              {venue.address.place}
-              <br />
-              {venue.address.direction}
-              <br />
-              {venue.address.city}
-            </address>
-          </div>
-        </div>
+        <H2>Ubicación</H2>
+        <iframe
+          src={venue.map}
+          height="300"
+          frameBorder="0"
+          allowFullScreen
+        />
       </Wrapper>
       <style jsx>{`
         iframe {
           border: none;
-          margin-right: 1em;
-          padding-right: 1em;
-          box-sizing: border-box;
-          width: 50%;
-        }
-        h2 {
-          font-size: 2em;
-          font-weight: 600;
-          margin-top: 0;
-        }
-        address {
-          font-style: normal;
-          font-size: 1.25em;
-          margin-bottom: 2em;
-        }
-        .content-wrapper {
-          max-width: 60em;
-          margin: 0 auto;
-          display: flex;
+          width: 70%;
         }
         @media (max-width: 30em) {
-          .content-wrapper {
-            flex-direction: column;
-          }
           iframe {
             width: 100%;
-            margin-right: 0;
-            padding-left: 2em;
-            padding-right: 2em;
-            margin-bottom: 2em;
-          }
-          address {
-            margin-bottom: 0;
-          }
-          .content {
-            padding: 0 2em;
           }
         }
       `}</style>
