e377c1ac87ac286c203cf9aefd2061e44ca78bbb
Sergio Xalambrí <sergio@able.co>
2019-01-05 12:23:37 -0500

    Add open graph design page

---
+++ pages/open-graph.js
@@ -0,0 +1,37 @@
+import Name from "../components/name";
+import Slogan from "../components/slogan";
+
+function OpenGraphPage() {
+  return (
+    <main>
+      <Name />
+      <Slogan animate={false} />
+
+      <style jsx global>{`
+        body {
+          font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto,
+            Oxygen, Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
+          margin: 0;
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          height: 100vh;
+          text-align: center;
+          font-size: 1.5rem;
+
+          background-color: white;
+          background-image: radial-gradient(
+            circle,
+            #d7d7d7,
+            #d7d7d7 1px,
+            #ffffff 1px,
+            #ffffff
+          );
+          background-size: 1em 1em;
+        }
+      `}</style>
+    </main>
+  );
+}
+
+export default OpenGraphPage;
