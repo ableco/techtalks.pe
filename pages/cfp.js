91a2ed296228d288ca4dad1c51c4450409ee3121
Sergio Xalambrí <sergio@able.co>
2019-01-08 17:22:19 -0500

    Update action and use GET method

--- pages/cfp.js
+++ pages/cfp.js
@@ -52,7 +52,7 @@ function CallForPapersPage() {
             ¡Si hay algo en lo que eres experto, o estás aprendiendo, o de lo
             que te gustaría contarnos, eres más que bienvenido a dar una charla!
           </Subtext>
-          <form name="cfp" action="/cfp/gracias" data-netlify="true" onSubmit={handleSubmit}>
+          <form name="cfp" method="GET" action="/cfp/gracias/" data-netlify="true" onSubmit={handleSubmit}>
             <fieldset>
               <legend>¿A qué email podríamos contactarte?</legend>
               <label htmlFor="email">
