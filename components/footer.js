881ba16fa40ad95297fc05fe97dbde6c14c6801c
Sergio Xalambrí <sergio@able.co>
2019-01-17 19:42:59 -0500

    Better footer styles

--- components/footer.js
+++ components/footer.js
@@ -1,4 +1,4 @@
-import Button from "./ui/button";
+import Link from "next/link";
 
 function Footer() {
   return (
@@ -6,13 +6,15 @@ function Footer() {
       <div>
         <strong className="name">Tech Talks</strong>
         <nav>
-          {/* <Button href="/blog" invert>Blog</Button> */}
-          <Button href="/cfp" invert>
-            Dar una charla
-          </Button>
-          <Button href="/coc" invert>
-            Código de Conducta
-          </Button>
+          {/* <Link href="/blog">
+            <a>Blog</a>
+          </Link> */}
+          <Link href="/cfp">
+            <a>Dar una charla</a>
+          </Link>
+          <Link href="/coc">
+            <a>Código de Conducta</a>
+          </Link>
         </nav>
       </div>
 
@@ -21,19 +23,51 @@ function Footer() {
           background-color: black;
           color: white;
           padding: 2rem 0;
+          font-size: 1.25rem;
         }
 
         footer > div {
           max-width: 64rem;
           margin: 0 auto;
+          padding: 0 1rem;
           display: flex;
-          justify-content: space-between;
-          align-items: center;
+          flex-direction: column;
         }
 
         .name {
-          font-size: 1.25rem;
+          font-size: 1.25em;
           letter-spacing: -0.05em;
+          display: block;
+          text-align: center;
+        }
+
+        a {
+          color: white;
+          text-decoration: underline;
+          display: block;
+          margin: 1rem 0;
+        }
+
+        nav {
+          display: block;
+        }
+
+        @media (min-width: 64rem) {
+          footer > div {
+            flex-direction: row;
+            justify-content: space-between;
+            align-items: center;
+          }
+
+          nav {
+            display: flex;
+            justify-content: space-between;
+          }
+
+          a {
+            display: inline-flex;
+            margin: 0 1rem;
+          }
         }
       `}</style>
     </footer>
