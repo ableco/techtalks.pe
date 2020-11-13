1e5a0a7b7cbb5e8758077524fe5f20f51cf1e021
Sergio Xalambrí <hello@sergiodxa.com>
2020-11-12 23:36:44 -0500

    Use new Tech Talks logo, change slogan and styles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import { FaDiscord } from "react-icons/fa";
 import { LinkButton } from "../components/button";
 import { Footer } from "../components/footer";
+import { TechTalks } from "../components/logo";
 import { Main } from "../components/main";
 import { Nav } from "../components/nav";
 
@@ -15,18 +16,17 @@ function HomePage({}: HomePageProps) {
       <Nav />
 
       <header className="container mx-auto px-4 flex justify-center items-center flex-col space-y-12">
-        <div className="space-y-4">
+        <div className="space-y-3">
           <h1 className="text-2xl sm:text-4xl text-black text-center leading-none tracking-tighter">
-            <span className="font-bold">Tech</span>{" "}
-            <span className="font-light">Talks</span>
+            <TechTalks className="inline-block h-3 sm:h-5 md:h-6 lg:h-8" />
           </h1>
           <h2
-            className="font-black text-3xl sm:text-6xl md:text-7xl lg:text-8xl xl:text-9xl text-black text-center tracking-tight"
+            className="font-extrabold text-2xl sm:text-5xl md:text-6xl lg:text-7xl text-black text-center tracking-tight"
             style={{ lineHeight: ".8" }}
           >
-            La comunidad de
+            Crecemos más
             <br />
-            tecnología del Perú
+            cuando compartimos
           </h2>
         </div>
         <div className="space-y-4 sm:space-x-4 text-center">
@@ -39,7 +39,7 @@ function HomePage({}: HomePageProps) {
           <LinkButton
             href="/discord"
             label={<span>Únete a la comunidad </span>}
-            className="bg-discord text-white border-discord hover:text-discord hover:bg-white"
+            className="bg-brand-discord text-white border-brand-discord hover:text-brand-discord hover:bg-white"
             icon={<FaDiscord className="w-6 h-6 inline-block" />}
           />
         </div>
